9228d43287d98d9e03fafaa54671cc13e4b6c8b5
kuba <j.kapuscik@abyssglass.pl>
2019-11-14 19:03:10 +0100

    Make imgs random

--- src/components/Index.js
+++ src/components/Index.js
@@ -5,18 +5,19 @@ import {MDBContainer, MDBCard} from "mdbreact";
 const Index = () => {
     return (
         <MDBContainer className={"content"}>
-            <MDBCard className={"col-12 p-3 mt-3"}>
-                <img src="https://mdbootstrap.com/img/Others/documentation/1.jpg" className="img-fluid col-12 p-0"
+            <MDBCard className={"col-12 p-3 mt-3 mb-3"}>
+                <img src="https://loremflickr.com/1920/1080" className="img-fluid col-12 p-0"
                      alt=""/>
             </MDBCard>
-            <MDBCard className={"col-12 p-3 mt-3"}>
-                <img src="https://mdbootstrap.com/img/Others/documentation/1.jpg" className="img-fluid col-12 p-0"
+            <MDBCard className={"col-12 p-3 mt-3 mb-3"}>
+                <img src="https://loremflickr.com/640/480" className="img-fluid col-12 p-0"
                      alt=""/>
             </MDBCard>
-            <MDBCard className={"col-12 p-3 mt-3"}>
-                <img src="https://mdbootstrap.com/img/Others/documentation/1.jpg" className="img-fluid col-12 p-0"
+            <MDBCard className={"col-12 p-3 mt-3 mb-3"}>
+                <img src="https://loremflickr.com/480/640" className="img-fluid col-12 p-0"
                      alt=""/>
             </MDBCard>
+
         </MDBContainer>
     )
 }
